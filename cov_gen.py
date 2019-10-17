66a4113fc93764066dfa9729447a414bb284b775
Ken Cycheng <ken.cycheng@gmail.com>
2019-10-17 13:50:16 +0800

    init commit

---
+++ cov_gen.py
@@ -0,0 +1,93 @@
+import sys
+from sv_cov import *
+import xlrd
+
+try:
+    table = xlrd.open_workbook(sys.argv[1])
+except:
+    print('File %s does not exist' % sys.argv[1])
+    exit()
+
+sheets = table.sheets()
+
+class FCOV:
+    def __init__(self, sheets):
+        self.sheets = sheets
+        self.line_state = 0
+
+    def print_sheet(self):
+	for sheet in self.sheets:
+	    for row in range(sheet.nrows):
+		if(row == 0):
+                    for col in range(sheet.ncols):
+                        if(sheet.cell(row,col).value=="LABEL"):
+                            self.label_col_num = col
+                        elif(sheet.cell(row,col).value=="TARGET"):
+                            self.target_col_num = col
+                        elif(sheet.cell(row,col).value=="IFF"):
+                            self.iff_col_num = col
+                        elif(sheet.cell(row,col).value=="BIN_TYPE"):
+                            self.bin_type_col_num = col
+                        elif(sheet.cell(row,col).value=="BIN_NAME"):
+                            self.bin_name_col_num = col
+                        elif(sheet.cell(row,col).value=="BIN_VAL"):
+                            self.bin_val_col_num = col
+                        elif(sheet.cell(row,col).value=="PREFIXS"):
+                            self.prefixs_col_num = col
+                    self.all_cp_cc = [];
+                else:
+                    label = sheet.cell(row,self.label_col_num).value
+                    if (('cp_' in label)or('cc_' in label)):
+                        if(self.line_state == 1 or self.line_state == 2):
+			    self.cp.gen_coverpoint()
+			    print(self.cp)
+			self.line_state = 1
+		        # finished consecutive structure
+			# process old structure
+			this_target = sheet.cell(row,self.target_col_num).value
+			this_iff = sheet.cell(row,self.iff_col_num).value
+			this_prefixs = sheet.cell(row,self.prefixs_col_num).value
+			# new a coverage structure
+			print("new cov struct -"+label)
+			self.cp = COVERPOINT(label, this_target, this_iff, this_prefixs)
+	       	    elif('FIN' in label):
+			self.line_state = 0
+			self.cp.gen_coverpoint()
+			print(self.cp)
+		    else:
+			self.line_state = 2
+						
+		    bin_type = sheet.cell(row,self.bin_type_col_num).value
+		    bin_name = sheet.cell(row,self.bin_name_col_num).value
+	            if(isinstance(sheet.cell(row,self.bin_val_col_num).value, float)):
+			bin_val = str(int(sheet.cell(row,self.bin_val_col_num).value))
+		    else:
+			bin_val = sheet.cell(row,self.bin_val_col_num).value
+
+		    # Handle the decoding of ' character. To deal with strings like 2'b10, 32'hffff
+		    bin_val = bin_val.replace(u'\u2019', u'\'').encode('ascii', 'ignore')
+
+		    if('M_' in bin_name):	
+			this_bin = PREDEF_BIN(bin_name, this_target, int(bin_val))
+			self.cp.add_predef_bins(this_bin)
+		    elif(bin_name !=''):						    
+			if(bin_type == ''):
+			    this_bin = COV_BIN(bin_name, bin_val)
+			else:
+			    this_bin = COV_BIN(bin_name, bin_val, bin_type)
+
+		        self.cp.add_bins(this_bin)
+
+
+
+	def __str__(self):
+	    return ("LABEL=%d, TARGET=%d, IFF=%d, BIN_NAME=%d, BIN_VAL+%d, PREFIXS=%d" % (self.label_col_num,
+		        self.target_col_num,
+			self.iff_col_num,
+			self.bin_name_col_num,
+			self.bin_val_col_num,
+			self.prefixs_col_num))
+
+fcov = FCOV(sheets)
+fcov.print_sheet()
+#print(fcov)
---
+++ sv_cov.py
@@ -0,0 +1,117 @@
+#
+# The base classes for generating systemverilog coverage model
+#
+class COV_STRUCT:
+    def __init__(self):
+        self.cov_struct_s = []
+
+    def __str__(self):
+        s=""
+        for lines in self.cov_struct_s:
+            s = s + lines + "\n"
+        return s
+class COV_BIN:
+    def __init__(self, bin_name, bin_val, bin_type = 'bins '):
+        self.bin_name = bin_name
+        self.bin_type = bin_type
+        self.bin_val = bin_val.replace('0x','\'h')
+        self.bin_val = self.bin_val.replace('0b','\'b')
+    def __str__(self):
+        with_keyword = 0
+        bins_keyword = ['binsof', 'interset', 'with']
+        for keyword in bins_keyword:
+            if(keyword in self.bin_val):
+                with_keyword = 1
+                break
+        if(with_keyword == 0):
+            self.bin_val = '{ ' + self.bin_val + ' }'
+
+        s =self.bin_type + ' %12s'% self.bin_name + ' = ' + self.bin_val + ';' 
+        return s
+
+class PREDEF_BIN:
+    def __init__(self, bin_type, *args, **keargs):
+        self.bin_type = bin_type
+        self.cov_bins = []
+        self.vector = []
+        if(bin_type == 'M_BITWISE'):
+            self.gen_bitwise(args[0], args[1])
+
+    def gen_bitwise(self, name_prefix, bit_num):
+        for i in range(bit_num):
+            self.vector.append('?') 
+        vector_prefix = str(bit_num)+'\'b'
+        for i in range(bit_num):
+            vector1 = list(self.vector)
+            vector1[bit_num -1-i] = '1'
+            one_bin = COV_BIN(name_prefix + str(i) + '_1', 
+                              vector_prefix + ''.join(vector1) ) 
+            vector1[bit_num -1-i] = '0'
+            zero_bin = COV_BIN(name_prefix + str(i) + '_0', 
+                              vector_prefix + ''.join(vector1) ) 
+            self.cov_bins.append(one_bin)
+            self.cov_bins.append(zero_bin)
+
+class COVERPOINT(COV_STRUCT):
+    def __init__(self, cp_name, target, iff='', prefix ='' ):
+        self.cp_name = cp_name
+        self.target = target 
+        self.iff = iff
+        self.prefix = prefix
+        self.cov_bins = []
+        self.options = []
+        self.predef_bins= []
+        COV_STRUCT.__init__(self) 
+
+    def add_bins(self, cov_bin): 
+        self.cov_bins.append(cov_bin)
+
+    def add_options(self, options):
+        self.cov_options.append(options)
+
+    def add_predef_bins(self, predef_bin):
+        for i in predef_bin.cov_bins:
+            self.cov_bins.append(i)
+    def gen_coverpoint(self): 
+        line1 = self.cp_name + ':' + ' coverpoint ' + self.prefix + self.target;
+
+        if(self.iff != ''):
+            line1 = line1 + ' iff('+ self.iff + ')'
+        if(len(self.cov_bins) == 0):
+            line1 = line1 + ';'
+        else:
+            line1 = line1 + ' {'
+
+        self.cov_struct_s.append(line1)
+
+        if(len(self.cov_bins)>0):
+            for ii in self.cov_bins:
+                self.cov_struct_s.append('  ' + str(ii))
+            self.cov_struct_s.append('} ')
+
+
+
+class CROSS(COV_STRUCT):
+    def __init__(self, cc_name, target):
+        self.cc_name = cc_name
+        self.target  = target
+        self.cov_bins = []
+        self.options = []
+        COV_STRUCT.__init__(self) 
+
+    def add_bins(self, cov_bin): 
+        self.cov_bins.append(cov_bin)
+
+    def gen_cross(self): 
+        line1 = self.cc_name + ':' + ' cross ' + self.target;
+        if(len(self.cov_bins) == 0):
+            line1 = line1 + ';'
+        else:
+            line1 = line1 + ' {'
+
+        self.cov_struct_s.append(line1)
+
+        if(len(self.cov_bins)>0):
+            for ii in self.cov_bins:
+                self.cov_struct_s.append('  ' + str(ii))
+            self.cov_struct_s.append('} ')
